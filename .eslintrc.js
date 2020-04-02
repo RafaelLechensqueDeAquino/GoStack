8ba7d4a3fd24b6238ac9250c4cf8c57fbd466187
Rafael Lechensque <rafaelaquino@alunos.utfpr.edu.br>
2020-04-02 20:07:50 -0300

    Criando a Migration e o Model de usuário

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,12 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
+    // "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassing": "off",
     "camelcase": "off",
     "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
-    
+
     "linebreak-style": 0,
   },
 };
--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'docker',
+  password: 'root',
   database: 'GoBarber',
   define: {
     timestamps: true,
